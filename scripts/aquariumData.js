c5e2f40e96221c9cd52fd9e0fbf43ade47f90bef
pascual ramirez Valeriano <pascualramirez27@gmail.com>
2024-05-30 11:42:33 -0400

    started fish filter

--- scripts/aquariumData.js
+++ scripts/aquariumData.js
@@ -6,14 +6,15 @@ export const database = {
             length: 3,
             location: "Great Barrier Reef",
             diet: "Omnivore",
-            image: "https://image.petmd.com/files/styles/863x625/public/2023-12/clownfish.jpg"},
+            image: "https://image.petmd.com/files/styles/863x625/public/2023-12/clownfish.jpg"
+        },
         {
             name: "Dory",
             species: "Blue Tang",
-            length: 5,
+            length: 6,
             location: "Great Barrier Reef",
             diet: "Omnivore",
-            image: "https://www.georgiaaquarium.org/wp-content/uploads/2018/09/powder-blue-tang-3.jpg" 
+            image: "https://www.georgiaaquarium.org/wp-content/uploads/2018/09/powder-blue-tang-3.jpg"
         },
         {
             name: "Marlin",
@@ -29,11 +30,12 @@ export const database = {
             length: 8,
             location: "Caribbean Sea",
             diet: "Herbivore",
-            image: "https://upload.wikimedia.org/wikipedia/commons/9/9f/Queen_Angelfish.jpg"  },
+            image: "https://upload.wikimedia.org/wikipedia/commons/9/9f/Queen_Angelfish.jpg"
+        },
         {
             name: "Swordfish",
             species: "Broadbill Swordfish",
-            length: 14,
+            length: 15,
             location: "Atlantic Ocean",
             diet: "Carnivore",
             image: "https://tropicfishhawaii.com/wp-content/uploads/2018/04/Swordfish.jpg"
@@ -53,14 +55,78 @@ export const database = {
             location: "North Atlantic Ocean",
             diet: "Omnivore",
             image: "https://www.pacificseafood.com/pac-incoming/2018/05/Atlantic_Salmon_Whole.jpg"
+        },
+        {
+            name: "Barracuda",
+            species: "Great Barracuda",
+            length: 18,
+            location: "Tropical Waters",
+            diet: "Carnivore",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtl4-Hx7RYQ-tlM3LnD-XwLzsKEdbV-zMdag&s"
+        },
+        {
+            name: "Seahorse",
+            species: "Common Seahorse",
+            length: 5,
+            location: "Shallow Tropical Waters",
+            diet: "Carnivore",
+            image: "https://inaturalist-open-data.s3.amazonaws.com/photos/310571/large.jpg"
+        },
+        {
+            name: "Pufferfish",
+            species: "Tetraodontidae",
+            length: 8,
+            location: "Warm Coastal Waters",
+            diet: "Omnivore",
+            image: "https://live.staticflickr.com/7315/13421084473_19dc339304_b.jpg"
+        },
+        {
+            name: "Lionfish",
+            species: "Pterois",
+            length: 12,
+            location: "Indo-Pacific",
+            diet: "Carnivore",
+            image: "https://lionfish.co/wp-content/uploads/2016/07/Volitans.jpg"
+        },
+        {
+            name: "Butterflyfish",
+            species: "Chaetodontidae",
+            length: 7,
+            location: "Tropical Reefs",
+            diet: "Omnivore",
+            image: "https://media.australian.museum/media/dd/images/Some_image.width-600.e801fde.jpg"
+        },
+        {
+            name: "Grouper",
+            species: "Epinephelinae",
+            length: 24,
+            location: "Warm Oceans",
+            diet: "Carnivore",
+            image: "https://upload.wikimedia.org/wikipedia/commons/1/11/Kirnja.jpg"
+        },
+        {
+            name: "Goldfish",
+            species: "Carassius auratus",
+            length: 5,
+            location: "Freshwater",
+            diet: "Omnivore",
+            image: "https://ncfishes.com/wp-content/uploads/2019/12/Carassius-auratus-Var-Gold.png"
+        },
+        {
+            name: "Moorish Idol",
+            species: "Zanclus cornutus",
+            length: 9,
+            location: "Indo-Pacific",
+            diet: "Omnivore",
+            image: "https://biogeodb.stri.si.edu/caribbean/resources/img/images/species/2146_12236.jpg"
         }
     ],
     tips: [
         {
             topic: "Water Temperature",
             text: "Maintain a consistent water temperature between 75-80°F (24-27°C) for most tropical fish."
-         },
-         {
+        },
+        {
             topic: "Water Quality",
             text: "Regularly test the water for pH, ammonia, nitrite, and nitrate levels to ensure a healthy environment for your fish."
         },
@@ -90,8 +156,8 @@ export const database = {
             name: "Great Barrier Reef",
             country: "Australia",
             description: "The Great Barrier Reef is the world's largest coral reef system, supporting a diverse array of marine life."
-         },
-         {
+        },
+        {
             name: "Galápagos Islands",
             country: "Ecuador",
             description: "The Galápagos Islands are renowned for their unique biodiversity and are home to many species not found anywhere else on Earth."
@@ -116,6 +182,5 @@ export const database = {
             country: "Palau",
             description: "Palau Reefs are renowned for their pristine coral gardens and abundant marine life, offering some of the best diving experiences in the world."
         }
-         
     ]
 }
